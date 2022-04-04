ddf000af5312fbdee7b0a34dcff931b8e1e235e8
hamzaabdous <abdoushamza6@gmail.com>
2022-04-04 13:05:39 +0000

    update service

--- src/main/java/com/example/testrestapi/config/CorsConfig.java
+++ src/main/java/com/example/testrestapi/config/CorsConfig.java
@@ -1,2 +1,13 @@
-package com.example.testrestapi.config;public class CorsConfig {
+package com.example.testrestapi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*").allowCredentials(false).allowedMethods("*").maxAge(3600).allowedHeaders("*");
+    }
 }
